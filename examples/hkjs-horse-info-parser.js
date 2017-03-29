a37a26aebe88dcbffb63fc9357155818aa3e20b7
ai.ppcheng <ai.ppcheng@gmail.com>
2017-03-28 23:02:49 -0400

    Add function to export data into csv format

--- examples/hkjs-horse-info-parser.js
+++ examples/hkjs-horse-info-parser.js
@@ -2,21 +2,51 @@ const fs      = require('fs');
 const cheerio = require('cheerio');
 
 function getHorseAttr(text, html) {
-  const result = html(`font:contains(${text})`)
+  const result = [];
+  const value = html(`font:contains(${text})`)
     .parent()
     .next()
     .text()
     .split(':')[1]
     .trim() || '';
 
-  console.log(`${text}: ${result}`);
+  const temp = text.split('/');
+  if (temp.length > 1) {
+    const valueArr = value.split('/');
+    for (let i = 0; i < temp.length; i++) {
+      result.push(valueArr[i].trim());
+      console.log(`${temp[i].trim()}: ${valueArr[i].trim()}`);
+    }
+  } else {
+    result.push(value);
+    console.log(`${text}: ${value}`);
+  }
+
   return result;
+}
+
+function save2xls(headers, records, filename) {
+  const writeStream = fs.createWriteStream(filename);
+
+  writeStream.write(`${headers.join(',')}\n`);
+
+  records.forEach((elem) => {
+    writeStream.write(`${elem.join(',')}\n`);
+  });
+
+  writeStream.on('finish', () => {
+    console.log('data has been saved successfully');
+    writeStream.close();
+  });
 }
 
 fs.readFile('example.html', (err, htmlString) => {
   if (err) return console.error(err);
 
-  const $ = cheerio.load(htmlString);
+  const headers = [];
+  const $       = cheerio.load(htmlString);
+
+  const records   = [];
 
   const title = $('title').text().split('-')[0].trim();
   console.log(`馬名: ${title}`);
@@ -33,17 +63,32 @@ fs.readFile('example.html', (err, htmlString) => {
   const stats                     = getHorseAttr('冠-亞-季-總出賽次數*', $);
   const numOver10PastRaceMeetings = getHorseAttr('最近十個賽馬日出賽場數', $);
 
+  $('tr > td[class="hsubheader"]').each((i, elem) => {
+    const value = $(elem).text().trim();
+    headers.push(value);
+    process.stdout.write(`${value} `);
+  });
+  process.stdout.write('\n');
+
   $('tr[bgcolor="#F8F4EF"], tr[bgcolor="#E7E4DF"]').each((i, elem) => {
     const numColumns = $(elem).children('td').length;
+
+    const row = [];
 
     for (let index = 0; index < numColumns; index++) {
       const line = $(elem).children('td').eq(index).text();
       if (line !== '') {
+        row.push(line.trim());
         process.stdout.write(`${line.trim()} `);
       }
     }
+    records.push(row);
+
     process.stdout.write('\n');
   });
 
+  save2xls(headers, records, 'example.csv');
+
   return 0;
 });
+
